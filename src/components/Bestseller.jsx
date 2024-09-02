1996812f704de23e82612a421e6e671a3e4668cb
GT x HAMZA <161149050+gthamza@users.noreply.github.com>
2024-09-02 15:44:21 +0500

    gt

--- src/components/Bestseller.jsx
+++ src/components/Bestseller.jsx
@@ -13,25 +13,25 @@ function Bestseller() {
 
   const products = [
     {
-      name: 'Krunch Burger',
+      name: 'KRUNCH BURGER',
       price: '290',
       image: 'https://www.kfcpakistan.com/images/b438e990-bc23-11ee-be0d-ed0e61ce8a3a-Untitleddesign(5)-min_variant_0-2024-01-26082002.png',
-      description: 'Krunch fillet, spicy mayo, lettuce, sandwiched between a sesame seed bun.'
+      description: 'KRUNCH FILLET, SPICY MAYO, LETTUCE, SANDWICHED BETWEEN A SESAME SEED BUN.'
     },
     {
-      name: 'Krunch Combo',
+      name: 'KRUNCH COMBO',
       price: '570',
       image: 'https://www.kfcpakistan.com/images/43a9fb50-ffaa-11ed-8180-812e571998fe-crunch-with-fries-and-drink-2023-05-31115706.png',
       description: 'Krunch Burger, fries, and a drink.'
     },
     {
-      name: 'Hot Wings Bucket',
+      name: 'HOT WINGS BUCKET',
       price: '650',
       image: 'https://www.kfcpakistan.com/images/38718ee0-bc17-11ee-97ca-ad9c0958c4fc-Plain-wings-min_variant_0-2024-01-26065041.png',
-      description: '10 pieces of Hot & Crispy Wings, packed with flavor and heat.'
+      description: '10 PIECES OF HOT & CRISPY WINGS, PACKED WITH FLAVOR AND HEAT.'
     },
     {
-      name: 'Mighty Zinger',
+      name: 'MIGHTY ZINGER',
       price: '750',
       image: 'https://www.kfcpakistan.com/images/33685b40-0461-11ee-911c-497570899609-Mighty_variant_0-2023-06-06115641.png',
       description: 'Double Zinger fillet with mayo, lettuce, and cheese, in a sesame seed bun.'
